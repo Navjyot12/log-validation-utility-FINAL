2302ad403dfb681727c27f1b3aaefc77594423a7
Navjyot <navjyots.dhaliwal@thewitslab.com>
2025-01-14 15:12:05 +0530

    Schema addons for  '@ondc/org/category'

--- schema/Retail/RET/on_select.ts
+++ schema/Retail/RET/on_select.ts
@@ -131,6 +131,10 @@ export const onSelectSchema = {
               items: {
                 type: 'object',
                 properties: {
+                  type: {
+                    type: 'string',
+                    enums:['Delivery', 'Self-Pickup', 'Buyer-Delivery']
+                  },
                   id: {
                     type: 'string',
                   },
@@ -142,6 +146,7 @@ export const onSelectSchema = {
                   },
                   '@ondc/org/category': {
                     type: 'string',
+                    enum: ['Takeaway', 'Kerbside', 'Immediate Delivery', 'Standard Delivery', 'Express Delivery', 'Same Day Delivery', 'Next Day Delivery']
                   },
                   '@ondc/org/TAT': {
                     type: 'string',
