e5891e3455c6685d8435c6c3ba658b86d3c2266f
Navjyot <navjyots.dhaliwal@thewitslab.com>
2025-01-30 02:30:33 +0530

    added np fees in confirm for items.tags

--- schema/Retail/RET/init.ts
+++ schema/Retail/RET/init.ts
@@ -179,8 +179,7 @@ export const initSchema = {
                           },
                         },
                       },
-
-                    }
+                    },
                   },
                 },
               },
